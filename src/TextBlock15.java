1ecd886f196f07b2965d57eff23218cdd9c6c336
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-05-23 15:31:42 -0500

    initial commit

---
+++ src/TextBlock15.java
@@ -0,0 +1,21 @@
+public class TextBlock15 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+
+
+        System.out.println("""
+                *
+                **
+                ***
+                ****
+                *****
+                               
+                """);
+    }
+
+}
