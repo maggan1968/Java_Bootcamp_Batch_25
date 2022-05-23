1e8fb982e1037cf29106c96e7a2d335529b00040
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-05-23 15:31:50 -0500

    initial commit

---
+++ src/OOP_Polymorphism/Polymorphism.java
@@ -0,0 +1,17 @@
+package OOP_Polymorphism;
+
+import inheritance_abstraction.BMW;
+import inheritance_abstraction.Car;
+import inheritance_abstraction.Tesla;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Car car = new Tesla(2019, "V3", "Black", 100000);
+        car.start();
+        ((Tesla)car).carStatic();
+        System.out.println("======================================");
+        car= new BMW(2019, "i8", "Black", 100000);
+        car.start();
+        ((BMW) car).carStatic();
+    }
+}
